60d83411ea277ddc14aafa8fdb938e9576fe9870
SiriusScaper <siriusscaper@gmail.com>
2019-05-29 01:51:25 -0700

    Added first admin command - Shutdown using process.exit()

---
+++ commands/admin/shutdown.js
@@ -0,0 +1,24 @@
+const { RichEmbed } = require('discord.js')
+const { light_red } = require('../../colors.json')
+
+module.exports = {
+  config: {
+    name: 'shutdown',
+    description: 'Shutdown the bot',
+    usage: '!shutdown',
+    authorized: 'Admin',
+    category: 'admin',
+    aliases: ['botstop'],
+  },
+  run: async (bot, message, args) => {
+
+    if (message.author.id != '124762196017152000') return message.channel.send('You are not the admin')
+
+    try {
+      await message.channel.send('Shutdown in 3...2...1')
+      process.exit()
+    } catch {
+      message.channel.send(`Error: ${e.message}`)
+    }
+  }
+}
