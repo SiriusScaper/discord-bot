fb76b39b88e76800ab28532d7a3e65c3faec4124
Ryan Uselmann <sirius@code-machine2.lan>
2020-03-20 18:34:11 -0700

    Added newvideo.js, still working on getting pubsub to work

--- events/client/newvideo.js
+++ events/client/newvideo.js
@@ -1,7 +1,32 @@
 /* eslint-disable no-console */
 const isToday = require('date-fns/is_today')
+const pubSubHubbub = require('pubsubhubbub')
+const http = require('http')
 
-console.log(isToday(new Date()))
+const PORT = 3000
+
+const server = http.createServer(handleRequest)
+
+function handleRequest(req, res) {
+}
+
+
+
+const pubSubSubscriber = pubSubHubbub.createServer()
+const topic = 'https://www.youtube.com/xml/feeds/videos.xml?channel_id=UC7W5LTUy7DJYpx0cpVKJe0g'
+const hub = 'http://pubsubhubbub.appspot.com/'
+
+pubSubSubscriber.on('subscribe', data => console.log(`${data.topic} subscribed`))
+
+pubSubSubscriber.on('listen', () => {
+  pubSubSubscriber.subscribe(topic, hub, err => {
+    if (err) {
+      console.log('Failed subscribing')
+    }
+  })
+})
+
 
 module.exports = bot => {
+
 }
